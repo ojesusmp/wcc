f9c83eb423f7c553165037a904b75c34dc58703b
ojesusmp <ojesusmp@gmail.com>
2026-02-04 07:23:32 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = ‘wcc-v14-pwa-cache-6’;
+const CACHE_NAME = 'wcc-v14-pwa-cache-7';
 const ASSETS = [
-‘./’,
-‘./index.html’,
-‘./manifest.json’,
-‘./icon-192.png’,
-‘./icon-512.png’
+'./',
+'./index.html',
+'./manifest.json',
+'./icon-192.png',
+'./icon-512.png'
 ];
 
-self.addEventListener(‘install’, (event) => {
+self.addEventListener('install', (event) => {
 event.waitUntil(
 caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
 );
 });
 
-self.addEventListener(‘activate’, (event) => {
+self.addEventListener('activate', (event) => {
 event.waitUntil(
 caches.keys().then((keys) =>
 Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
@@ -21,10 +21,9 @@ Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
 );
 });
 
-self.addEventListener(‘fetch’, (event) => {
+self.addEventListener('fetch', (event) => {
 const req = event.request;
-// Cache-first for same-origin GET requests
-if (req.method === ‘GET’ && new URL(req.url).origin === self.location.origin) {
+if (req.method === 'GET' && new URL(req.url).origin === self.location.origin) {
 event.respondWith(
 caches.match(req).then((cached) => {
 if (cached) return cached;
