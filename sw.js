d9e9078946d2d8b6f1eb6f415dc13bb2e7b5171c
ojesusmp <ojesusmp@gmail.com>
2026-02-05 18:31:19 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-const CACHE_NAME='wcc-v14-pwa-cache-10';
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json']))
-  );
-  self.skipWaiting();
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
+const CACHE='wcc-v15-cache-1';
+const URLS=['./','./index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(resp.status===200){const c=resp.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return resp}).catch(()=>caches.match('./'))))});
